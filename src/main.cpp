3959d42abbee631ce06949734ed7555488f0913a
William Shiao <willshiao@gmail.com>
2017-05-01 15:53:26 -0700

    Add main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,18 @@
-int main() {
-  return 0;
+#include <string>
+#include <vector>
+#include "header/Shell.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  // Convert argv to string vector
+  vector<string> args(argc);
+  for(int i = 0; i < argc; ++i) {
+    args.at(i) = argv[i];
+  }
+
+  Shell s(args);
+  s.run();
+
+  return 1;  // Non-zero exit code if exit command not used
 }
