87300deb6a3c1f3a0b914473793d3b95a5ae3358
William Shiao <willshiao@gmail.com>
2017-05-09 23:18:35 -0700

    Write exit test
    Fixes #12

--- tests/exit.sh
+++ tests/exit.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-# TODO: Tests exit and commands with exit
+./setup.sh
+
+../bin/rshell < input/exit.txt &> exit_output.txt
+if cmp -s exit_output.txt expected/exit_output.txt; then
+  echo "Exit Test Passed.";
+else
+  echo "Exit Test FAILED!";
+fi
+rm -rf exit_output.txt
+
+./cleanup.sh
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,3 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$DIR"
 ./commented_command.sh
 ./multi_command.sh
+./exit.sh
