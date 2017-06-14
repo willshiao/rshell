willshiao/rshell

commit 9ed7dab232fe3696749414a55a2fa00ab6e108e7

Add parsing for InputRedirector

src/Command.cpp
@@ -23,7 +23,7 @@ StatusCode Command::eval() {
   if(this->args.at(0) == "exit") {
     exit(0);
   } else if(this->args.at(0) == "test") {
-    return TestCommand::runCommand(this->args);
+    return this->runCommand(this->args);
   }
   StatusCode s = Command::runCommand(this->args);
   #ifdef DEBUG
@@ -44,6 +44,15 @@ StatusCode Command::runCommand(vector<string>& args) {
   pid_t pid = fork();
   int commandStatus;
 
+  if(hasPipe()) {
+    cout << "Output redirection detected" << endl;
+    if(pipe(this->pipefd) == -1) {
+      cout << "Error creating pipe." << endl;
+      delete[] argv;
+      return IO_ERROR;
+    }
+  }
+
   if(pid == 0) {  // Child process
     if(execvp(argv[0], argv) < 0) {
       cout << "No command \"" << argv[0] << "\" found." << endl;
@@ -53,6 +62,7 @@ StatusCode Command::runCommand(vector<string>& args) {
   } else if(pid > 0) {  // Parent process
     waitpid(pid, &commandStatus, 0);
     delete[] argv;
+
     if(WEXITSTATUS(commandStatus) == 0) return SUCCESS;
     #ifdef DEBUG
       cout << "Unknown error!" << endl;

src/InputRedirector.cpp
@@ -1,0 +1,14 @@
+#include "header/Command.h"
+#include "header/InputRedirector.h"
+
+using namespace std;
+
+StatusCode InputRedirector::eval() {
+  Command *leftCommand = static_cast<Command*>(this->left);
+  Command *rightCommand = static_cast<Command*>(this->right);
+
+  #ifdef DEBUG
+    cout << "Piping input from " << rightCommand->getArgs().front() << endl;
+  #endif
+  return leftCommand->eval();
+}

src/header/Command.h
@@ -11,10 +11,16 @@ using namespace std;
 class Command : public Base {
  protected:
   vector<string> args;
+  string inputFile, outputFile;  // Optional input/output files for streams
+  bool appendOutput;
+
  public:
-  static StatusCode runCommand(vector<string>& args);
+  int pipefd[2];  // File descriptor for pipe, [0] is read and [1] is write
+
+  StatusCode runCommand(vector<string>& args);
   Command() {}
-  explicit Command(vector<string> a) : args(a) {
+  explicit Command(vector<string> a)
+      : args(a), inputFile(""), outputFile(""), appendOutput(false) {
     #ifdef DEBUG
       cout << "New command created with args: ";
       for(auto str : a) {
@@ -23,9 +29,19 @@ class Command : public Base {
       cout << endl;
     #endif
   }
+
   virtual StatusCode eval();
   virtual bool isEmpty();
+
+  vector<string> getArgs() { return args; }
+  void setOutputFile(string newOut) { outputFile = newOut; }
+  void setInputFile(string newIn) { inputFile = newIn; }
+  void setAppend(bool newState) { appendOutput = newState; }
+
+ private:
+  bool hasPipe() { return !(inputFile.empty() && outputFile.empty());  }
 };
+
 
 class NullCommand : public Command {
  public:
@@ -35,9 +51,10 @@ class NullCommand : public Command {
   virtual bool isEmpty() { return true; }
 };
 
+
 class TestCommand : public Command {
  public:
-  static StatusCode runCommand(vector<string>& args);
+  StatusCode runCommand(vector<string>& args);
   explicit TestCommand(vector<string> a) : Command(a) { }
 };
 

src/Shell.cpp
@@ -13,6 +13,7 @@
 #include "header/CommandConnector.h"
 #include "header/AndConnector.h"
 #include "header/OrConnector.h"
+#include "header/InputRedirector.h"
 
 using namespace std;
 
@@ -151,13 +152,17 @@ Base* Shell::parseCommand(string line) {
         operands.push(applyOperator(op, left, right));
       }
       if(!operators.empty() && operators.top() == "(") operators.pop();
+
     } else if(isOperator(word)) {
       #ifdef DEBUG
         cout << "Current word is operator: " << word << endl;
       #endif
-      while(!operators.empty() && operators.top() != "(") {
+
+      while(!operators.empty() && operators.top() != "("
+          && !hasLowerPrecedence(operators.top(), word)) {
         string op = operators.top();
         operators.pop();
+
         #ifdef DEBUG
           cout << "(2) Got operator: " << op << endl;
         #endif
@@ -202,20 +207,28 @@ Base* Shell::applyOperator(const string& op, Base* left, Base* right) {
       cout << "Created new OrConnector" << endl;
     #endif
     return new OrConnector(left, right);
-  } else if(op == "&&") {
+  }
+  if(op == "&&") {
     #ifdef DEBUG
       cout << "Created new AndConnector" << endl;
     #endif
     return new AndConnector(left, right);
   }
+  if(op == ";") {
+    #ifdef DEBUG
+      cout << "Created new CommandConnector" << endl;
+    #endif
+    return new CommandConnector(left, right);
+  }
+
   #ifdef DEBUG
-    cout << "Created new CommandConnector" << endl;
+    cout << "Created new InputRedirector" << endl;
   #endif
-  return new CommandConnector(left, right);
+  return new InputRedirector(left, right);
 }
 
 bool Shell::isOperator(const string &s) {
-  return s == "||" || s == "&&" || s == ";";
+  return s == "||" || s == "&&" || s == ";" || s == "<";
 }
 
 bool Shell::isParens(const string &s) {
@@ -224,4 +237,14 @@ bool Shell::isParens(const string &s) {
 
 bool Shell::isSymbol(const string &s) {
   return Shell::isOperator(s) || Shell::isParens(s);
+}
+
+bool Shell::hasLowerPrecedence(const std::string &s1, const std::string &s2) {
+  return getPrecedence(s1) < getPrecedence(s2);
+}
+
+unsigned Shell::getPrecedence(const std::string &s) {
+  if(isParens(s)) return 0;
+  if(s == "||" || s == "&&" || s == ";") return 1;
+  return 2;
 }

src/header/InputRedirector.h
@@ -1,0 +1,14 @@
+#ifndef RSHELL_INPUT_REDIRECTOR_H
+#define RSHELL_INPUT_REDIRECTOR_H
+
+#include <vector>
+#include <string>
+#include "Connector.h"
+
+class InputRedirector : public Connector {
+ public:
+  InputRedirector(Base* l, Base* r) : Connector(l, r) {}
+  virtual StatusCode eval();
+};
+
+#endif

src/header/Shell.h
@@ -15,6 +15,8 @@ class Shell {
   static bool isOperator(const std::string &s);
   static bool isParens(const std::string &s);
   static bool isSymbol(const std::string &s);
+  static bool hasLowerPrecedence(const std::string &s1, const std::string &s2);
+  static unsigned getPrecedence(const std::string &s);
 };
 
 #endif

commit 85233706ca0f0b6495d62d61e9dff380d800c7f0

Fix TestCommand, which was broken in 9ed7dab

src/Command.cpp
@@ -23,9 +23,9 @@ StatusCode Command::eval() {
   if(this->args.at(0) == "exit") {
     exit(0);
   } else if(this->args.at(0) == "test") {
-    return this->runCommand(this->args);
+    return (new TestCommand(this->args))->eval();
   }
-  StatusCode s = Command::runCommand(this->args);
+  StatusCode s = this->runCommand(this->args);
   #ifdef DEBUG
     if(s != SUCCESS) cout << "Invalid command." << endl;
   #endif
@@ -99,4 +99,8 @@ StatusCode TestCommand::runCommand(vector<string>& args) {
   }
   cout << (status ? "(True)" : "(False)") << endl;
   return (status ? SUCCESS : IO_ERROR);
+}
+
+StatusCode TestCommand::eval() {
+  return this->runCommand(this->args);
 }

src/InputRedirector.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 StatusCode InputRedirector::eval() {
   Command *leftCommand = static_cast<Command*>(this->left);
-  Command *rightCommand = static_cast<Command*>(this->right);
 
   #ifdef DEBUG
+    Command *rightCommand = static_cast<Command*>(this->right);
     cout << "Piping input from " << rightCommand->getArgs().front() << endl;
   #endif
   return leftCommand->eval();

src/header/Command.h
@@ -54,8 +54,10 @@ class NullCommand : public Command {
 
 class TestCommand : public Command {
  public:
-  StatusCode runCommand(vector<string>& args);
   explicit TestCommand(vector<string> a) : Command(a) { }
+
+  StatusCode runCommand(vector<string>& args);
+  virtual StatusCode eval();
 };
 
 #endif